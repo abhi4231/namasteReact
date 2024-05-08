352d5bf467112ab73a1aea54777b05c20f753e8d
ProfileApp <profileapp.lcs@gmail.com>
2024-05-08 21:32:32 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React !");
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    [React.createElement(
+    "div", 
+    {id: "child1"},
+    [React.createElement("h1", {}, "I am a h1 tag"), 
+    React.createElement("h2", {}, "I am a h2 tag")
+]),
+React.createElement(
+    "div", 
+    {id: "child2"},
+    [React.createElement("h1", {}, "I am a h1 tag"), 
+    React.createElement("h2", {}, "I am a h2 tag")
+])
+]
+
+);
+
+console.log(parent); //this is an object 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); //render converts this object into the h1 tag element.
