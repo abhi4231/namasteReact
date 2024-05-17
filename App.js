1dce0542e60e58280a9da37b06b34e16c190cc2b
abhijitnaikwadi <abhijitrnaikwadi@gmail.com>
2024-05-17 10:13:14 +0530

    JSX first implementation

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React !");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    [React.createElement(
-    "div", 
-    {id: "child1"},
-    [React.createElement("h1", {}, "I am a h1 tag"), 
-    React.createElement("h2", {}, "I am a h2 tag")
-]),
-React.createElement(
-    "div", 
-    {id: "child2"},
-    [React.createElement("h1", {}, "I am a h1 tag"), 
-    React.createElement("h2", {}, "I am a h2 tag")
-])
-]
+// React.createElement => object => HTMLElement(render)
 
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from Abhijit !"
 );
 
-console.log(parent); //this is an object 
+const jsxHeading = (
+  <h1 id="heading" tabIndex="5">
+    Hello From JSX
+  </h1>
+);
+
+console.log(jsxHeading); //this is an object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //render converts this object into the h1 tag element.
+root.render(jsxHeading); //render converts this object into the h1 tag element.
